7a6f6983c89093de9aaadf8e430d3108e9ea88a2
Duy Nguyen <zuikng@gmail.com>
2017-03-25 10:54:14 -0400

    Python within template

--- happy_tweet/__init__.py
+++ happy_tweet/__init__.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name = "Zui"
+
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html", blah={
+        'h': "Hello, " + name,
+        'y': "and beyond",
+        't': "Orochimaru"
+    })
 
 @app.route("/blah/")
 def blah():
